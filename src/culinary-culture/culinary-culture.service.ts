42be48d1aa80b81f619384bf52b08aaf6acc3226
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2024-11-02 00:06:35 -0500

    Solve culinary culture code smells

--- src/culinary-culture/culinary-culture.service.ts
+++ src/culinary-culture/culinary-culture.service.ts
@@ -10,64 +10,31 @@ import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
 @Injectable()
 export class CulinaryCultureService {
-  // Code smell 1: Public variable que debería ser privada
-  public cacheKey: string = 'culinary-culture';
 
-  // Code smell 2: Variable no utilizada
-  private unusedVariable: string = 'unused';
-
-  // Code smell 3: Nombre de variable poco descriptivo
-  private x: number = 0;
+  cacheKey: string = "culinary-culture";
 
   constructor(
     @InjectRepository(CulinaryCultureEntity)
     private readonly culinaryCultureRepository: Repository<CulinaryCultureEntity>,
 
     @Inject(CACHE_MANAGER)
-    private readonly cacheManager: Cache,
+       private readonly cacheManager: Cache
   ) {}
 
-  // Code smell 4: Método duplicado con ligeras variaciones
   async findAll(): Promise<CulinaryCultureEntity[]> {
-    const cached: CulinaryCultureEntity[] = await this.cacheManager.get<
-      CulinaryCultureEntity[]
-    >(this.cacheKey);
-
-    if (!cached) {
-      const culinaryCultures: CulinaryCultureEntity[] =
-        await this.culinaryCultureRepository.find({
-          relations: ['countries', 'products', 'recipes', 'restaurants'],
-        });
-      await this.cacheManager.set(this.cacheKey, culinaryCultures);
-      return culinaryCultures;
+    const cached: CulinaryCultureEntity[] = await this.cacheManager.get<CulinaryCultureEntity[]>(this.cacheKey);
+      
+    if(!cached){
+        const culinaryCultures: CulinaryCultureEntity[] = await this.culinaryCultureRepository.find({
+          relations: ['countries', 'products', 'recipes', 'restaurants'],})
+        await this.cacheManager.set(this.cacheKey, culinaryCultures);
+        return culinaryCultures;
     }
 
     return cached;
   }
 
-  // Code smell 5: Método muy similar al anterior (duplicación)
-  async findAllDuplicate(): Promise<CulinaryCultureEntity[]> {
-    const cached: CulinaryCultureEntity[] = await this.cacheManager.get<
-      CulinaryCultureEntity[]
-    >(this.cacheKey);
-
-    if (!cached) {
-      const culinaryCultures: CulinaryCultureEntity[] =
-        await this.culinaryCultureRepository.find({
-          relations: ['countries', 'products', 'recipes'],
-        });
-      await this.cacheManager.set(this.cacheKey, culinaryCultures);
-      return culinaryCultures;
-    }
-
-    return cached;
-  }
-
-  // Code smell 6: Parámetro no utilizado
-  async findOne(
-    id: string,
-    unusedParam: string,
-  ): Promise<CulinaryCultureEntity> {
+  async findOne(id: string): Promise<CulinaryCultureEntity> {
     const culinaryCulture: CulinaryCultureEntity =
       await this.culinaryCultureRepository.findOne({
         where: { id },
@@ -82,50 +49,13 @@ export class CulinaryCultureService {
     return culinaryCulture;
   }
 
-  // Vulnerabilidad: SQL Injection por concatenación directa
-  async findByName(name: string): Promise<CulinaryCultureEntity[]> {
-    return await this.culinaryCultureRepository.query(
-      'SELECT * FROM culinary_culture WHERE name = ' + name,
-    );
-  }
-
-  // Code smell 7: Método con demasiados parámetros
   async create(
     culinaryCulture: CulinaryCultureEntity,
-    param1: string,
-    param2: string,
-    param3: string,
-    param4: string,
-    param5: string,
   ): Promise<CulinaryCultureEntity> {
-    // Code smell 8: Variable innecesaria
-    const unnecessary = culinaryCulture;
-    return await this.culinaryCultureRepository.save(unnecessary);
+    return await this.culinaryCultureRepository.save(culinaryCulture);
   }
 
-  // Code smell 9: Comentarios innecesarios
   async update(
-    id: string,
-    culinaryCulture: CulinaryCultureEntity,
-  ): Promise<CulinaryCultureEntity> {
-    // Esta línea busca la cultura culinaria
-    const persistedCulinaryCulture: CulinaryCultureEntity =
-      await this.culinaryCultureRepository.findOne({ where: { id } });
-    // Comprueba si existe
-    if (!persistedCulinaryCulture)
-      throw new BusinessLogicException(
-        'The culinary culture with the given id was not found',
-        BusinessError.NOT_FOUND,
-      );
-    // Guarda los cambios
-    return await this.culinaryCultureRepository.save({
-      ...persistedCulinaryCulture,
-      ...culinaryCulture,
-    });
-  }
-
-  // Code smell 10: Método duplicado innecesario
-  async updateSonar(
     id: string,
     culinaryCulture: CulinaryCultureEntity,
   ): Promise<CulinaryCultureEntity> {
